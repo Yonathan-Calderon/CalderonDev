f3c5e18c2a15d6472f59ad45e3611530477a9294
Yonathan-Calderon <calderonprog@gmail.com>
2022-07-23 08:10:08 -0300

    Modificacion en Cotacto

---
+++ enviar.php
@@ -0,0 +1,21 @@
+<?php  
+
+// Llamando a los campos
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$mensaje = $_POST['mensaje'];
+
+// Datos para el correo
+$destinatario = "contacto@calderondev.com";
+$asunto = "Contacto desde nuestra web";
+
+$carta = "De: $nombre \n";
+$carta .= "Correo: $correo \n";
+$carta .= "Telefono: $telefono \n";
+$carta .= "Mensaje: $mensaje";
+
+// Enviando Mensaje
+mail($destinatario, $asunto, $carta);
+header('Location:index.html');
+
+?>
